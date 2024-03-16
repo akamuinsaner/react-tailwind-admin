7077bc72ef508004f11fbd0fd8e54970b2275ff4
wangshuai <15108387504@163.com>
2024-03-16 19:59:14 +0800

    add prettier

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,36 +1,30 @@
 'use client'
-import ScrollPage from "@/src/components/ScrollPage";
-import Page from "@/src/components/Page";
-import Nav from "./pageSec/Nav";
-import Side from "./pageSec/Side";
-import Content from "@/src/components/Content";
-import { ReactNode } from "react";
-import { GlobalContext, IGlobalContext } from './globalContext';
-import { useReducer } from 'react';
-import { reducer, setThemeAction, initialState } from './globalStore';
+import ScrollPage from '@/src/components/ScrollPage'
+import Page from '@/src/components/Page'
+import Nav from './pageSec/Nav'
+import Side from './pageSec/Side'
+import Content from '@/src/components/Content'
+import { ReactNode } from 'react'
+import { GlobalContext, IGlobalContext } from './globalContext'
+import { useReducer } from 'react'
+import { reducer, setThemeAction, initialState } from './globalStore'
 
-export default function App({
-    children
-}: {
-    children: ReactNode
-}) {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const setTheme = (theme: string) => dispatch(setThemeAction(theme));
+export default function App({ children }: { children: ReactNode }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const setTheme = (theme: string) => dispatch(setThemeAction(theme))
   const contextValue: IGlobalContext = {
     theme: state.theme,
-    setTheme: setTheme
+    setTheme: setTheme,
   }
   return (
     <GlobalContext.Provider value={contextValue}>
-    <ScrollPage>
-      <Page className="pt-20 pl-64">
-        <Nav />
-        <Side />
-        <Content>
-            {children}
-        </Content>
-      </Page>
-    </ScrollPage>
+      <ScrollPage>
+        <Page className='pt-20 pl-64'>
+          <Nav />
+          <Side />
+          <Content>{children}</Content>
+        </Page>
+      </ScrollPage>
     </GlobalContext.Provider>
-  );
+  )
 }
