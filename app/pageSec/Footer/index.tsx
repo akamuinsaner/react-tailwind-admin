5ecfd9ff1c726eb85761ebe9ea6c0674adbce597
wangshuai <15108387504@163.com>
2024-04-06 10:48:32 +0800

    add some icons

--- app/pageSec/Footer/index.tsx
+++ app/pageSec/Footer/index.tsx
@@ -1,5 +1,10 @@
 import Flex from '@/src/components/Flex';
 import Text from '@/src/components/Text';
+import GithubIcon from '@/app/utils/icons/GithubIcon';
+import Link from 'next/link';
+import { GITHUBLINK, LNKEDINLINK, PORTFOLIOLINK } from '@/app/utils/constants';
+import LinkedinIcon from '@/app/utils/icons/LinkedinIcon';
+import { GlobeAltIcon } from '@heroicons/react/24/solid';
 
 const Footer = () => {
     return (
@@ -7,7 +12,20 @@ const Footer = () => {
             className='w-full py-4 px-8 text-mainText text-center'
             align='center'
             direction='column'
+            gap='medium'
         >
+            <Flex gap='medium'>
+                <Link href={GITHUBLINK} target='__blank'>
+                    <GithubIcon className='h-6 w-6 text-mainText' />
+                </Link>
+                <Link href={LNKEDINLINK} target='__blank'>
+                    <LinkedinIcon className='h-6 w-6 text-mainText' />
+                </Link>
+                <Link href={PORTFOLIOLINK} target='__blank'>
+                    <GlobeAltIcon className='h-6 w-6 text-mainText' />
+                </Link>
+            </Flex>
+
             <Text>
                 RTA is an admin template for building large scale web
                 applications,powered by NextJs, Tailwindcss,made by akamuinsaner
--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,1 +1,7 @@
 export const host = `http://localhost:3001`;
+
+export const GITHUBLINK = 'https://github.com/akamuinsaner';
+
+export const LNKEDINLINK = 'https://www.linkedin.com/in/elcidwang/';
+
+export const PORTFOLIOLINK = 'https://codingtheworld.site/';
